c04a9330685067fb1acf6619793a6acf16f24157
Dbeehe <cjh123456a@naver.com>
2023-09-22 15:39:33 +0900

    학생정보등록

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.StudentDTO;
+import com.example.demo.service.StudentService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Controller
+@RequiredArgsConstructor
+public class StudentController {
+    private final StudentService studentService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute StudentDTO studentDTO) {
+        studentService.save(studentDTO);
+        return "index";
+    }
+
+    @GetMapping("/students")
+    public String findAll(Model model) {
+        List<StudentDTO> studentDTOList = studentService.findAll();
+        model.addAttribute("studentList", studentDTOList);
+        return "list";
+    }
+
+    @GetMapping("/student/{id}")
+    public String finById(@PathVariable("id") Long id, Model model){
+        try {
+            StudentDTO studentDTO = studentService.findById(id);
+            model.addAttribute("student", studentDTO);
+            return "detail";
+        }catch (NoSuchElementException e) {
+            return "NotFound";
+        }
+    }
+
+}
+
+
+
+
+
+
